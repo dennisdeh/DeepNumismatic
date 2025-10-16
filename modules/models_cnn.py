fb59819d5b29b9b707dbb991ae68d255d87a95b5
deh <dennishansendeh@gmail.com>
2025-10-16 23:26:01 +0200

    added new modules for non-image models and CNNs with documentation

---
+++ modules/models_cnn.py
@@ -0,0 +1,5 @@
+"""
+This module contains CNN models obtained in two different ways:
+    New CNN trained from scratch
+    Fine-tuning of an existing model (https://docs.pytorch.org/tutorials/beginner/transfer_learning_tutorial.html)
+"""
